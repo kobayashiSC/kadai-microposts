e400bdf14159215f0f9f37e3a2a174a8ac9e8d1a
SC.kobayashi <xiaolin.photestyou@gmail.com>
2023-11-30 09:27:40 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UsersController; //追記
+use App\Http\Controllers\MicropostsController; //追記
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +16,9 @@ use App\Http\Controllers\UsersController; //追記
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', [MicropostsController::class,"index"]);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [MicropostsController::class,"index"])->middleware(["auth"])->name("dashboard");
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -33,4 +30,5 @@ require __DIR__.'/auth.php';
 
 Route::group(["middleware" => ["auth"]], function(){
     Route::resource("users", UsersController::class, ["only" => ["index","show"]]);
+    Route::resource("microposts", MicropostsController::class,["only" => ["store","destroy"]]);
 });
