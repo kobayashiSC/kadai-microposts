eac42b82e4c3b4de6afec95a78778ee846b61f67
SC.kobayashi <xiaolin.photestyou@gmail.com>
2023-11-30 06:40:54 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UsersController; //追記
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::group(["middleware" => ["auth"]], function(){
+    Route::resource("users", UsersController::class, ["only" => ["index","show"]]);
+});
