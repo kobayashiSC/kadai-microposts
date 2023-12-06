d5f8d7819c36b8a073d8e826254b59708eccb064
SC.kobayashi <xiaolin.photestyou@gmail.com>
2023-12-06 09:34:25 +0000

    add logout

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -8,7 +8,7 @@
     <li><a class="link link-hover" href="{{ route("users.favorites", Auth::user()->id) }}">Favorites</a></li>
     <li class="divider lg:hidden"></li>
     {{--ログアウトへのリンク--}}
-    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest("form").submit();">Logout</a></li>
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
 @else
     {{--ユーザ登録ページへのリンク--}}
     <li><a class="link link-hover" href="{{ route("register") }}">Signup</a></li>
